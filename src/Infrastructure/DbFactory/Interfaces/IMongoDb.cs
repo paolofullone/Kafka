0d231eceaabaa43575a1a30c14f36511a543e26d
paolo.fullone <paolo.enrico@gmail.com>
2025-03-22 15:19:57 -0300

    oracle and mongodb for future use

---
+++ src/Infrastructure/DbFactory/Interfaces/IMongoDb.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Driver;
+
+namespace Infrastructure.DbFactory.Interfaces
+{
+    interface IMongoDb
+    {
+        string GetConnectionString();
+        IMongoDatabase CreateConnection();
+    }
+}
---
+++ src/Infrastructure/DbFactory/Interfaces/IOracleDb.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Infrastructure.DbFactory.Interfaces
+{
+    interface IOracleDb
+    {
+        string GetConnectionString();
+        IDbConnection GetConnection();
+    }
+}
