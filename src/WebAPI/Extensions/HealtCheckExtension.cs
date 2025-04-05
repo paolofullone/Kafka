3ce8aeb88d4a6aa6eb33ad5c518518f78a036b4b
paolo.fullone <paolo.enrico@gmail.com>
2025-04-05 19:29:34 -0300

    refactor healthchecks

--- src/WebAPI/Extensions/HealtCheckExtension.cs
+++ src/WebAPI/Extensions/HealtCheckExtension.cs
@@ -9,22 +9,29 @@ namespace WebApi.Extensions
     {
         public static IServiceCollection AddApplicationHealthChecks(this IServiceCollection services, IConfiguration configuration)
         {
+
             var kafkaConfig = configuration.GetSection("KafkaSettings").Get<ProducerConfig>();
             var kafkaTopic = configuration.GetSection("KafkaSettings:Topics:KafkaHealthCheck:Name").Get<string>();
+            var sqlServer = configuration.GetConnectionString("SqlServer")!;
+            var oracle = configuration.GetConnectionString("Oracle")!;
+
+            var databaseQueries = new Dictionary<string, (string ConnectionString, IList<string> Queries, string Provider)>
+            {
+                { "SqlServer", (sqlServer, new List<string> { "SELECT 1 FROM dbo.SAMPLE_MESSAGES" }, "SqlServer" ) },
+                { "Oracle", (oracle, new List<string> { "" +
+                    "SELECT 1 FROM ADMIN.SAMPLE_MESSAGES",
+                    "SELECT 1 FROM ADMIN.WO_ACCESS"
+                }, "Oracle") }
+            };
+
+            services.AddSingleton(new HealthCheckExtensionMultiDatabase(databaseQueries));
 
             services.AddHealthChecks()
-                .AddCheck("live", check: () => HealthCheckResult.Healthy(), tags: new[] { "live", "ready" }, TimeSpan.FromSeconds(30))
-                .AddKafka(kafkaConfig!, kafkaTopic!, name: "Kafka")
-                .AddSqlServer(
-                    configuration.GetConnectionString("SqlServer")!,
-                    name: "SqlServer",
-                    failureStatus: HealthStatus.Unhealthy,
-                    tags: new[] { "db", "mssql" })
-                .AddOracle(
-                configuration.GetConnectionString("Oracle")!,
-                name: "Oracle",
-                failureStatus: HealthStatus.Unhealthy,
-                tags: new[] { "db", "oracle" });
+                    .AddCheck(name: "live", check: () => HealthCheckResult.Healthy(), tags: new[] { "live", "ready" }, TimeSpan.FromSeconds(30))
+                    .AddKafka(kafkaConfig!, kafkaTopic!, name: "Kafka")
+                    .AddSqlServer(sqlServer, name: "SqlServer", failureStatus: HealthStatus.Unhealthy, tags: new[] { "db", "mssql" })
+                    .AddOracle(oracle, name: "Oracle", failureStatus: HealthStatus.Unhealthy, tags: new[] { "db", "oracle" })
+                    .AddCheck<HealthCheckExtensionMultiDatabase>(name: "MultiDatabase", failureStatus: HealthStatus.Unhealthy, tags: new[] { "db", "mssql", "oracle" });
 
             return services;
         }
@@ -33,11 +40,11 @@ namespace WebApi.Extensions
         {
             endpoints.MapHealthChecks("health/ready", new HealthCheckOptions
             {
-                Predicate = (check) => 
-                    check.Tags.Contains("ready") || 
-                    check.Tags.Contains("db") || 
-                    check.Tags.Contains("mssql") || 
-                    check.Tags.Contains("Kafka") || 
+                Predicate = (check) =>
+                    check.Tags.Contains("ready") ||
+                    check.Tags.Contains("db") ||
+                    check.Tags.Contains("mssql") ||
+                    check.Tags.Contains("Kafka") ||
                     check.Tags.Contains("oracle"),
                 ResponseWriter = WriteCustomResponse
             });
---
+++ src/WebAPI/Extensions/HealthCheckExtensionMultiDatabase.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+
+namespace WebApi.Extensions
+    // can be move do infra, so the webapi does not deal with dapper
+{
+    public class HealthCheckExtensionMultiDatabase : IHealthCheck
+    {
+        private readonly IDictionary<string, (string connectionString, IList<string> Queries, string Provider)> _databasesQueries;
+
+        public HealthCheckExtensionMultiDatabase(IDictionary<string, (string connectionString, IList<string> Queries, string provider)> databasesQueries)
+        {
+            _databasesQueries = databasesQueries;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var healthCheckResults = new Dictionary<string, HealthCheckResult>();
+
+            foreach (var dbQuery in _databasesQueries)
+            {
+                foreach (var query in dbQuery.Value.Queries)
+                {
+                    try
+                    {
+                        using (var connection = CreateConnection(dbQuery.Value.connectionString, dbQuery.Value.Provider))
+                        {
+                            await connection.OpenAsync(cancellationToken);
+
+                            using (var command = CreateCommand(query, connection, dbQuery.Value.Provider))
+                            {
+                                await command.ExecuteScalarAsync(cancellationToken);
+                                healthCheckResults[$"{dbQuery.Key}-{query}"] = HealthCheckResult.Healthy("ok");
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        var errorMessage = Regex.Unescape($"Exception during query execution: {ex.Message}");
+                        healthCheckResults[$"{dbQuery.Key}-{query}"] = HealthCheckResult.Unhealthy(errorMessage);
+                    }
+                }
+            }
+
+            var overallStatus = healthCheckResults.Values.All(result => result.Status == HealthStatus.Healthy) ? HealthStatus.Healthy : HealthStatus.Unhealthy;
+            return new HealthCheckResult(overallStatus, description: "Multi-database health check", data: healthCheckResults.ToDictionary(kvp => kvp.Key, kvp => (object)kvp.Value.Description!)!);
+        }
+
+        private DbConnection CreateConnection(string connectionString, string provider)
+        {
+            return provider switch
+            {
+                "SqlServer" => new SqlConnection(connectionString),
+                "Oracle" => new OracleConnection(connectionString),
+                _ => throw new NotSupportedException($"Provider {provider} is not supported.")
+            };
+        }
+
+        private DbCommand CreateCommand(string query, DbConnection connection, string provider)
+        {
+            return provider switch
+            {
+                "SqlServer" => new SqlCommand(query, (SqlConnection)connection),
+                "Oracle" => new OracleCommand(query, (OracleConnection)connection),
+                _ => throw new NotSupportedException($"Provider {provider} is not supported.")
+            };
+        }
+    }
+}
